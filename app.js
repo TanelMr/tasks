bdea628b4e5a14125793a095852b9f31098af444
Tanel Märjama <tanel.marjama@khk.ee>
2021-10-22 10:38:45 +0300

    Added js to submit tasks and link to delete them

---
+++ app.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const taskInput = document.querySelector("#task");
+form.addEventListener("submit", addTask)
+console.log (form, taskInput);
+
+function addTask (event) {
+    //input value
+    const task = taskInput.value;
+    // create <li> element
+    const li = document.createElement("li");
+    // define <li> CSS class
+    li.classname = "collection-item";
+    // create text value for <li>
+    const tekst = document.createTextNode(task);
+    console.log(tekst)
+    // add text value to <li>
+    li.appendChild(tekst);
+    //create link element
+    const link = document.createElement("a");
+    //add href
+    link.setAttribute("href", "#");
+    // add CSS to link
+    link.className = "secondary-content";
+    // add "X" text to link
+    link.appendChild(document.createTextNode("X"));
+    // add link to <li>
+    li.appendChild(link);
+    // find <ul> DOM component
+    const ul = document.querySelector(".collection");
+    // add <li> to <ul>
+    ul.appendChild(li);
+    // clear input
+    taskInput.value = "";
+
+    event.preventDefault();
+}
